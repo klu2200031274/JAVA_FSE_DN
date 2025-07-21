231452c91d1cbf420700799f92f56054cf16521e
klu2200031274 <159106083+klu2200031274@users.noreply.github.com>
2025-07-21 18:37:30 +0530

    Delete Ex1_Configuring a Basic Spring Application directory

--- Ex1_Configuring a Basic Spring Application/Codes/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-    public void fetchBooks() {
-        System.out.println("[Bean Method] BookRepository: Fetching books from the database...");
-    }
-}
--- Ex1_Configuring a Basic Spring Application/Codes/BookService.java
+++
@@ -1,7 +0,0 @@
-package com.library.service;
-
-public class BookService {
-    public void displayBooks() {
-        System.out.println("[Bean Method] BookService: Displaying books.");
-    }
-}
